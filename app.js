81ab54662a7adb941093055150b323033ed2fe79
Steven <ldh921126@gmail.com>
2023-09-27 18:29:53 +0900

    가져온 뉴스 리스트를 클릭하면, 리스트 하단에 클릭한 해당 글의 제목을 출력해 보기

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
+const container = document.getElementById('root');
 let ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const CONTENTS_URL ='https://api.hnpwa.com/v0/item/@id.json'
+
 
 ajax.open('GET',NEWS_URL, false)
 ajax.send();
 
-const newFeed = JSON.parse(ajax.response);
+const newsFeed = JSON.parse(ajax.response);
 const ul= document.createElement('ul');
+
+window.addEventListener('hashchange',()=>{
+    const id =location.hash.substring(1);
+
+    const title = document.createElement('h1');
+    content.appendChild(title);
+
+    ajax.open('GET',CONTENTS_URL.replace('@id',id),false)
+    ajax.send();
+
+    const newsContent =JSON.parse(ajax.response);
+
+    title.innerHTML = newsContent.title
+})
 
 
 for(let i=0; i<10; i++){
+    const a =document.createElement('a');
     const li= document.createElement('li');
 
-    li.innerHTML=newFeed[i].title;
+    a.href =`#${newsFeed[i].id}`
+    a.innerHTML=`${newsFeed[i].title}(${newsFeed[i].comments_count})`;
+
+    li.appendChild(a);
     ul.appendChild(li);
 }
-
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
