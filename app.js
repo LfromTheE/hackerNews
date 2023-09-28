0c50b5073b274508233d71d7cb21f076f877fb27
Steven <ldh921126@gmail.com>
2023-09-28 16:50:30 +0900

    남아 있던 dom api를 템플릿 리터럴로 구현 후, 글 목록 읽기 기능과 글 읽기 기능을 모듈화 하여 함수로 바꿈. router 함수를 통해 해쉬 값의 변화에 따른 분기를 나눠 화면 전환되도록 함.

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENTS_URL ='https://api.hnpwa.com/v0/item/@id.json'
 
-getData = (url)=>{
+
+const ul= document.createElement('ul');
+
+function getData(url) {
     ajax.open('GET',url, false)
     ajax.send();
     
@@ -12,33 +15,51 @@ getData = (url)=>{
 }
 
 
-const newsFeed = getData(NEWS_URL);
-const ul= document.createElement('ul');
+function newsFeed(){
 
-window.addEventListener('hashchange',function() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList =[];
+    newsList.push('<ul>');
+
+    for(let i=0; i<10; i++){
+        newsList.push(
+        `
+        <li>
+            <a href="#${newsFeed[i].id}">
+            ${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
+        </li>
+        `
+        )
+    }
+    newsList.push('</ul>');
+    container.innerHTML = newsList.join("");
+
+}
+
+function newsDetail() {
     const id =location.hash.substring(1);
-    const title = document.createElement('h1');
-
-    content.appendChild(title);
     const newsContent =getData(CONTENTS_URL.replace('@id',id));
 
-    title.innerHTML = newsContent.title
-})
-
-
-for(let i=0; i<10; i++){
-    const a =document.createElement('a');
-    const li= document.createElement('li');
-    const div = document.createElement('div')
-
-    div.innerHTML=
+    container.innerHTML =
     `
-    <li>
-        <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
-    </li>
+     <h1>${newsContent.title}</h1>
+
+     <div>
+        <a href="#">목록으로</a>
+     </div>
     `
-    ul.appendChild(div);
+
 }
-container.appendChild(ul);
-container.appendChild(content);
+
+function router(){
+    const routePath = location.hash;
+    if(routePath==''){
+        newsFeed();
+    }else{
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange',router)
+router();
+
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,29 +123,37 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENTS_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-getData = function getData(url) {
+var ul = document.createElement('ul');
+function getData(url) {
   ajax.open('GET', url, false);
   ajax.send();
   return JSON.parse(ajax.response);
-};
-var newsFeed = getData(NEWS_URL);
-var ul = document.createElement('ul');
-window.addEventListener('hashchange', function () {
-  var id = location.hash.substring(1);
-  var title = document.createElement('h1');
-  content.appendChild(title);
-  var newsContent = getData(CONTENTS_URL.replace('@id', id));
-  title.innerHTML = newsContent.title;
-});
-for (var i = 0; i < 10; i++) {
-  var a = document.createElement('a');
-  var li = document.createElement('li');
-  var div = document.createElement('div');
-  div.innerHTML = "\n    <li>\n        <a href=\"#".concat(newsFeed[i].id, "\">\n        ").concat(newsFeed[i].title, "(").concat(newsFeed[i].comments_count, ")</a>\n    </li>\n    ");
-  ul.appendChild(div);
 }
-container.appendChild(ul);
-container.appendChild(content);
+function newsFeed() {
+  var newsFeed = getData(NEWS_URL);
+  var newsList = [];
+  newsList.push('<ul>');
+  for (var i = 0; i < 10; i++) {
+    newsList.push("\n        <li>\n            <a href=\"#".concat(newsFeed[i].id, "\">\n            ").concat(newsFeed[i].title, "(").concat(newsFeed[i].comments_count, ")</a>\n        </li>\n        "));
+  }
+  newsList.push('</ul>');
+  container.innerHTML = newsList.join("");
+}
+function newsDetail() {
+  var id = location.hash.substring(1);
+  var newsContent = getData(CONTENTS_URL.replace('@id', id));
+  container.innerHTML = "\n     <h1>".concat(newsContent.title, "</h1>\n\n     <div>\n        <a href=\"#\">\uBAA9\uB85D\uC73C\uB85C</a>\n     </div>\n    ");
+}
+function router() {
+  var routePath = location.hash;
+  if (routePath == '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+window.addEventListener('hashchange', router);
+router();
 },{}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -171,7 +179,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55468" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58454" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
