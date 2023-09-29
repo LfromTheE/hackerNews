ab4de9064f566fd9f477e5bc8fa1959d31f46466
Steven <ldh921126@gmail.com>
2023-09-29 22:40:14 +0900

    리스트의 현 페이지 위치를 전역변수로 설정하고, 이를 이용하여 페이지 위치 상태를 유지. 이를 통해 게시글을 클릭하고 돌아간 후에도 페이지를 기억한 위치로 돌아갈 수 있도록 구현하였음. 라우터 또한 기존과 다르게 요청이 페이지 전환인지, 게시글 상세 읽기 요청인지를 분활하여 라우터에서 응답 할 수 있도록 변경 함

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENTS_URL ='https://api.hnpwa.com/v0/item/@id.json'
 
+const store ={
+    currentPage : 1,
 
-const ul= document.createElement('ul');
+}
 
 function getData(url) {
     ajax.open('GET',url, false)
@@ -21,23 +23,28 @@ function newsFeed(){
     const newsList =[];
     newsList.push('<ul>');
 
-    for(let i=0; i<10; i++){
+    for(let i=(store.currentPage-1)*10; i<store.currentPage*10; i++){
         newsList.push(
         `
         <li>
-            <a href="#${newsFeed[i].id}">
+            <a href="#/show/${newsFeed[i].id}">
             ${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
         </li>
         `
         )
     }
     newsList.push('</ul>');
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage-1>0?store.currentPage-1:1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage < newsFeed.length / 10 ? store.currentPage + 1 : newsFeed.length / 10}">다음 페이지</a>
+        </div>
+        `);
     container.innerHTML = newsList.join("");
 
 }
-
 function newsDetail() {
-    const id =location.hash.substring(1);
+    const id =location.hash.substring(7);
     const newsContent =getData(CONTENTS_URL.replace('@id',id));
 
     container.innerHTML =
@@ -45,7 +52,7 @@ function newsDetail() {
      <h1>${newsContent.title}</h1>
 
      <div>
-        <a href="#">목록으로</a>
+        <a href="#/page/${store.currentPage}">목록으로</a>
      </div>
     `
 
@@ -54,6 +61,9 @@ function newsDetail() {
 function router(){
     const routePath = location.hash;
     if(routePath==''){
+        newsFeed();
+    }else if(routePath.indexOf('#/page/')>=0){
+        store.currentPage=Number(routePath.substring(7));
         newsFeed();
     }else{
         newsDetail();
