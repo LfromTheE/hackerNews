7027f9636b9505e0e6ec85fdfc17774274c1d785
Steven <ldh921126@gmail.com>
2023-09-30 19:18:03 +0900

    읽은 게시물을 전역변수로 관리하여 각 게시물의 읽은 상태를 보관하여, 게시글 리스트에서 색의 변화로 알 수 있게끔 구현

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const CONTENTS_URL ='https://api.hnpwa.com/v0/item/@id.json'
 
 const store ={
     currentPage : 1,
-
+    feeds : [],  
 }
 
 function getData(url) {
@@ -16,11 +16,22 @@ function getData(url) {
     return JSON.parse(ajax.response)
 }
 
+function makeFeeds(feeds){
+    for(let i=0; i<feeds.length; i++){
+        feeds[i].read = false;
+    }
+    return feeds;
+}
+
 
 function newsFeed(){
-
-    const newsFeed = getData(NEWS_URL);
     const newsList =[];
+    let newsFeed = store.feeds
+
+    if (newsFeed.length==0){
+        newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    }
+
     let template =
     `
 <div class="bg-gray-600 min-h-screen">
@@ -48,7 +59,7 @@ function newsFeed(){
     `;
     for(let i=(store.currentPage-1)*10; i<store.currentPage*10; i++){
         newsList.push(
-            `      <div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+            `<div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
             <div class="flex">
               <div class="flex-auto">
                 <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
@@ -107,6 +118,13 @@ function newsDetail() {
 
     </div>
   </div>`;
+
+  for(let i=0; i<store.feeds.length;i++){
+    if(store.feeds[i].id === Number(id)){
+        store.feeds[i].read = true;
+        break;
+    }
+  }
 
   function makeComment(comments, called=0) {
     const commentString =[];
