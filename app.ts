8ddbae3acc981fca32a2572360776b3004050581
Steven <ldh921126@gmail.com>
2023-10-05 11:04:50 +0900

    이전에 타입 알리아스로 작성된 코드를 인터페이스를 이용해 상속 형태로 변경, 함수 getData또한 인터페이스를 이용하여 하나의 상위 클래스와 두개의 하위 클래스로 나누어, 각 사용에 따라 다른 클래스를 인스턴스로 생성할 수 있도록 변경, 상위 클래스의 getRequest 메소드가 호출하는 쪽에 노출 되지 않도록 접근제한자를 적용함

--- app.ts
+++ app.ts
@@ -1,30 +1,30 @@
-type Store = {
+interface Store {
   currentPage: number;
   feeds: NewsFeed[];
 }
 
-type News = {
-  id: number;
-  time_ago: string;
-  title: string;
-  url: string;
-  user:string;
-  content: string;
+interface News {
+  readonly id: number;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly url: string;
+  readonly user:string;
+  readonly content: string;
 }
 
-type NewsFeed = News & {
-  comments_count: number;
-  points: number;
+interface NewsFeed extends News {
+  readonly comments_count: number;
+  readonly points: number;
   read?: boolean;
 }
 
-type NewsDetail = News & {
-  comments: NewsComment[];
+interface NewsDetail extends News {
+  readonly comments: NewsComment[];
 }
 
-type NewsComment = News & {
-  comments: [];
-  level: number;
+interface NewsComment extends News{
+  readonly comments: [];
+  readonly level: number;
 }
 
 const container : HTMLElement | null = document.getElementById('root');
@@ -38,10 +38,33 @@ const store: Store ={
     feeds : [],  
 }
 
-function getData<AjaxResponse>(url: string): AjaxResponse{
-    ajax.open('GET',url, false)
-    ajax.send();
-    return JSON.parse(ajax.response)
+class Api {
+  url: string;
+  ajax: XMLHttpRequest;
+  constructor(url: string){
+    this.url = url;
+    this.ajax = new XMLHttpRequest();
+  }
+
+  protected getRequest<AjaxResponse>(): AjaxResponse{
+    this.ajax.open('GET',this.url, false)
+    this.ajax.send();
+
+    return JSON.parse(this.ajax.response)
+  }
+}
+
+
+class NewFeedApi extends Api{
+  getData(): NewsFeed[] {
+    return this.getRequest<NewsFeed[]>()
+  }
+}
+
+class NewsDetailApi extends Api{
+  getData(): NewsDetail {
+    return this.getRequest<NewsDetail>()
+  }
 }
 
 function makeFeeds(feeds: NewsFeed[]){
@@ -61,73 +84,75 @@ function updateView(html: string): void{
 
 
 function newsFeed(): void{
-    const newsList =[];
-    let newsFeed: NewsFeed[] = store.feeds
+  const api = new NewFeedApi(NEWS_URL);
+  const newsList =[];
+  let newsFeed: NewsFeed[] = store.feeds
 
-    if (newsFeed.length==0){
-        newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
-    }
+  if (newsFeed.length==0){
+      newsFeed = store.feeds = makeFeeds(api.getData());
+  }
 
-    let template =
-    `
-<div class="bg-gray-600 min-h-screen">
-    <div class="bg-white text-xl">
-      <div class="mx-auto px-4">
-        <div class="flex justify-between items-center py-6">
-          <div class="flex justify-start">
-            <h1 class="font-extrabold">Hacker News</h1>
-          </div>
-          <div class="items-center justify-end">
-            <a href="#/page/{{__prev_page__}}" class="text-gray-500">
-              Previous
-            </a>
-            <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
-              Next
-            </a>
-          </div>
-        </div> 
+  let template =
+      `
+  <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                Previous
+              </a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                Next
+              </a>
+            </div>
+          </div> 
+        </div>
       </div>
-    </div>
-    <div class="p-4 text-2xl text-gray-700">
-      {{__news_feed__}}        
-    </div>
-</div>
+      <div class="p-4 text-2xl text-gray-700">
+        {{__news_feed__}}        
+      </div>
+  </div>
     `;
-    for(let i=(store.currentPage-1)*10; i<store.currentPage*10; i++){
-        newsList.push(
-            `<div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
-            <div class="flex">
-              <div class="flex-auto">
-                <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
-              </div>
-              <div class="text-center text-sm">
-                <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
-              </div>
+  for(let i=(store.currentPage-1)*10; i<store.currentPage*10; i++){
+      newsList.push(
+          `<div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+          <div class="flex">
+            <div class="flex-auto">
+              <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
             </div>
-            <div class="flex mt-3">
-              <div class="grid grid-cols-3 text-sm text-gray-500">
-                <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
-                <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
-                <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
-              </div>  
+            <div class="text-center text-sm">
+              <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
             </div>
-          </div>    `
-        )
-    }
+          </div>
+          <div class="flex mt-3">
+            <div class="grid grid-cols-3 text-sm text-gray-500">
+              <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+              <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+              <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+            </div>  
+          </div>
+        </div>    `
+      )
+  }
 
-    template =template.replace('{{__news_feed__}}',newsList.join(''));
-    template =template.replace('{{__prev_page__}}',String(store.currentPage-1>0?store.currentPage-1:1));
-    template =template.replace('{{__next_page__}}',String(store.currentPage < newsFeed.length / 10 ? store.currentPage + 1 : newsFeed.length / 10));
-    
-    updateView(template)    
+  template =template.replace('{{__news_feed__}}',newsList.join(''));
+  template =template.replace('{{__prev_page__}}',String(store.currentPage-1>0?store.currentPage-1:1));
+  template =template.replace('{{__next_page__}}',String(store.currentPage < newsFeed.length / 10 ? store.currentPage + 1 : newsFeed.length / 10));
+  
+  updateView(template)    
     
 }
 
 
 function newsDetail(): void {
-    const id =location.hash.substring(7);
-    const newsContent =getData<NewsDetail>(CONTENTS_URL.replace('@id',id));
-    let template = 
+  const id =location.hash.substring(7);
+  const api = new NewsDetailApi(CONTENTS_URL.replace('@id',id));
+  const newsContent =api.getData();
+  let template = 
     `    <div class="bg-gray-600 min-h-screen pb-8">
     <div class="bg-white text-xl">
       <div class="mx-auto px-4">
